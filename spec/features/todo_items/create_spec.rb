06f496d0fd4e7b6eb3673f1c2745c8ac0c17ee09
Gabriele Rossi <g2m.rossi@gmail.com>
2015-03-10 14:32:30 +0000

    Added spec helpers

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -3,13 +3,6 @@ require 'spec_helper'
 describe "Adding todo items" do
 	let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
 
-	def visit_todo_list(list)
-		visit "/todo_lists"
-		within "#todo_list_#{list.id}" do
-			click_link "List Items"
-		end
-	end
-
 	it "is successful with valid content" do
 		visit_todo_list(todo_list)
 		click_link "New Todo Item"
@@ -22,7 +15,7 @@ describe "Adding todo items" do
 
 	end
 
-	it "displays an error with no conten" do
+	it "displays an error with no content" do
 		visit_todo_list(todo_list)
 		click_link "New Todo Item"
 		fill_in "Content", with: ""
--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -4,12 +4,6 @@ describe "Viewing todo items" do
 
 	let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
 
-	def visit_todo_list(list)
-		visit "/todo_lists"
-		within "#todo_list_#{list.id}" do
-			click_link "List Items"
-		end
-	end
 
 	# before do
 	# 	visit "/todo_lists"
