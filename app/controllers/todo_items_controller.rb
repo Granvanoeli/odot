b43fde87762407db88a9ab50cbbf2cb411b4a08e
Gabriele Rossi <g2m.rossi@gmail.com>
2015-03-10 19:13:09 +0000

    Added the mark as completefunctionality and spec

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -23,6 +23,12 @@ class TodoItemsController < ApplicationController
   	end
   end
 
+  def complete
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to todo_list_todo_items_path, notice: "Todo item was marked as complete"
+  end
+
   def edit
     @todo_item = @todo_list.todo_items.find(params[:id])
 
